5878aa31fed883331c07ca6876d141fd1faf4569
sdxrh2005 <sdxrh2005@163.com>
2025-01-19 12:00:40 +0800

    更新主分支的输出文本

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,31 +1,23 @@
 package com;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("Git 练习项目 - 第二个版本");
+        System.out.println("Git 练习项目 - main 分支修改");
 
         calculator(10, 5);
 
-        multiply(10, 5);
-
     }
+
 
 
     public static void calculator(int a, int b) {
 
-        System.out.println("两数之和: " + (a + b));
-
-    }
-
-
-    public static void multiply(int a, int b) {
-
-        System.out.println("两数之积: " + (a * b));
+        System.out.println("求和结果: " + (a + b));
 
     }
 
 }
+
 
 
