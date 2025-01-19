c22f9bf0a85ec7b927eff5f969c9cb99fd78c776
sdxrh2005 <sdxrh2005@163.com>
2025-01-19 11:52:02 +0800

    初始化项目，添加基础计算器功能

---
+++ src/com/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git 练习项目 - main 分支修改");
+        calculator(10, 5);
+    }
+
+    public static void calculator(int a, int b) {
+        System.out.println("求和结果: " + (a + b));
+    }
+}
